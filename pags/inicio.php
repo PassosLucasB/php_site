7de2830c959d0457949c4762f394943f9ef6f55d
PassosLucasB <PassosL@outlook.com>
2022-08-25 06:07:32 -0300

    create_site_model

---
+++ pags/inicio.php
@@ -0,0 +1,11 @@
+<form method="POST">
+    <br>
+    <label>Para acessar o site faça seu login, insira o email cadastrado:</label>
+    <input type="text" class="form-control"><br>
+
+    <label>Insira a senha cadastrada:</label>
+    <input type="password" class="form-control"><br>
+
+    <input type="submit" value="Entrar" class="btn btn-primary btn-lg btn-block">
+    <p align="right"><a href="?pagina=recuperar">Esqueceu a senha? Recupere já!</a></p>
+</form>
